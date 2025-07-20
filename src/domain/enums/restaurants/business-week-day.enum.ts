e45e605d7da0ea509df70eec33f663483d026cc3
Vitor Bastos <vbss-services@outlook.com>
2025-07-20 16:40:28 -0300

    feat: add restaurants domain enum

---
+++ src/domain/enums/restaurants/business-week-day.enum.ts
@@ -0,0 +1,11 @@
+export enum BusinessWeekDay {
+  MONDAY = 'monday',
+  TUESDAY = 'tuesday',
+  WEDNESDAY = 'wednesday',
+  THURSDAY = 'thursday',
+  FRIDAY = 'friday',
+  SATURDAY = 'saturday',
+  SUNDAY = 'sunday'
+}
+
+export const businessWeekDayValues = Object.values(BusinessWeekDay)
