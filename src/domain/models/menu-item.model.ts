53a4e1eb597d2921f8c17119feedabb2bfa71729
Vitor Bastos <vbss-services@outlook.com>
2025-07-29 16:28:52 -0300

    feat: menu items model with optionals

---
+++ src/domain/models/menu-item.model.ts
@@ -0,0 +1,21 @@
+export interface MenuItemOptional {
+  name: string
+  maxQuantity?: number
+  price: number
+}
+
+export interface MenuItem {
+  id: string
+  name: string
+  description?: string
+  categoryId: string
+  restaurantId: string
+  price: number
+  stock: number
+  discount: number
+  medias: string
+  optionals: MenuItemOptional[]
+  isActive: boolean
+  createdAt: string
+  updatedAt: string
+}
