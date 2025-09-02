1fd1f35ac13e726cfade68cfd3c1df4e3d18cdf1
Vitor Bastos <vbss-services@outlook.com>
2025-09-02 15:59:31 -0300

    feat: remove unnecessary loading from client context

--- src/presentation/contexts/client-context.tsx
+++ src/presentation/contexts/client-context.tsx
@@ -3,7 +3,6 @@ import type { Client } from '@/domain/models/client.model'
 
 export interface ClientContextData {
   client: Client | null
-  isLoading: boolean
   error: string | null
   setClient: (client: Client) => void
   clearClient: () => void
