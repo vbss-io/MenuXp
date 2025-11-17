e93c47f32b31c5d29c37316f7d9d79c4d4c43c98
Vitor Bastos <vbss-services@outlook.com>
2025-11-17 18:37:22 -0300

    chore: add global lint:fix and apply lint

--- apps/server/src/@customers/application/whatsapp-verification/verify-code/verify-code.usecase.ts
+++ apps/server/src/@customers/application/whatsapp-verification/verify-code/verify-code.usecase.ts
@@ -1,6 +1,6 @@
-import { inject } from '@api/infra/dependency-injection/registry';
-import type { VerificationCodeService } from '@customers/application/whatsapp-verification/verification-code.service';
-import type { VerifyCodeType } from '@customers/application/whatsapp-verification/verify-code/verify-code.schema';
+import { inject } from '@api/infra/dependency-injection/registry'
+import type { VerificationCodeService } from '@customers/application/whatsapp-verification/verification-code.service'
+import type { VerifyCodeType } from '@customers/application/whatsapp-verification/verify-code/verify-code.schema'
 
 export class VerifyCodeUsecase {
   @inject('VerificationCodeService')
