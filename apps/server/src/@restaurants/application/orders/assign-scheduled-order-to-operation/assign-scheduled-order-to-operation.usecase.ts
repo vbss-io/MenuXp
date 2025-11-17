1386d4c5e9ce64c36cfc37afcefae7b95b8ff999
Vitor Bastos <vbss-services@outlook.com>
2025-11-17 18:12:06 -0300

    feat: server dispare order change status event

--- apps/server/src/@restaurants/application/orders/assign-scheduled-order-to-operation/assign-scheduled-order-to-operation.usecase.ts
+++ apps/server/src/@restaurants/application/orders/assign-scheduled-order-to-operation/assign-scheduled-order-to-operation.usecase.ts
@@ -1,8 +1,11 @@
 import { ForbiddenError } from '@api/domain/errors/forbidden.error'
 import { NotFoundError } from '@api/domain/errors/not-found.error'
+import { Queue } from '@api/infra/adapters/queue/queue.adapter'
 import { inject } from '@api/infra/dependency-injection/registry'
 import { OperationStatus } from '@restaurants/domain/operations/enums/operation-status.enum'
+import { ORDER_STATUS_CHANGED } from '@restaurants/domain/orders/consts/order-events.const'
 import { OrderStatus } from '@restaurants/domain/orders/enums/order-status.enum'
+import { OrderStatusChanged } from '@restaurants/domain/orders/events/order-status-changed.event'
 import { OperationRepository } from '@restaurants/infra/repositories/operation.repository'
 import { OrderRepository } from '@restaurants/infra/repositories/orders.repository'
 import { RestaurantRepository } from '@restaurants/infra/repositories/restaurant.repository'
@@ -15,6 +18,9 @@ export interface AssignScheduledOrderToOperationUsecaseInput {
 }
 
 export class AssignScheduledOrderToOperationUsecase {
+  @inject('Queue')
+  private readonly Queue!: Queue
+
   @inject('UserRepository')
   private readonly UserRepository!: UserRepository
 
@@ -47,7 +53,20 @@ export class AssignScheduledOrderToOperationUsecase {
     if (operation.restaurantId !== order.restaurantId) {
       throw new ForbiddenError('Operation is not for this restaurant')
     }
+    const oldStatus = order.status
     order.assignToOperation(operationId)
     await this.OrderRepository.update({ id: orderId }, order)
+    if (oldStatus !== order.status) {
+      const orderStatusChangedEvent = new OrderStatusChanged({
+        orderId,
+        orderCode: order.code,
+        restaurantId: order.restaurantId,
+        customerId: order.clientId,
+        customerName: order.customer.name,
+        oldStatus,
+        newStatus: order.status
+      })
+      await this.Queue.publish(ORDER_STATUS_CHANGED.eventName, orderStatusChangedEvent.data)
+    }
   }
 }
